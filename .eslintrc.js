45bd3e0717d6483ec8db90cc010fbf197785b7ec
Igor Moraes <igormoraesdev@gmail.com>
2021-11-12 23:51:32 +0000

    chore: fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,35 @@ module.exports = {
     node: true,
     jest: true,
   },
+  parser: '@typescript-eslint/parser',
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'universe/native',
     'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'eslint-config-prettier',
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
-    ecmaVersion: 2021,
-  },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
-  rules: {
-    indent: ['error', 2, {SwitchCase: 1}],
-    quotes: ['error', 'single', {avoidEscape: true}],
-    'no-empty-function': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'react/display-name': 'off',
-    'react/prop-types': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-  },
+  plugins: ['@typescript-eslint'],
   settings: {
-    react: {
-      version: 'detect',
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    indent: ['error', 2],
   },
 };

78d281e55760ddbc3d187c075923b586adf2d400
Igor Moraes <igormoraesdev@gmail.com>
2021-11-15 04:25:10 +0000

    feat: add global style

---
+++ src/presentation/styles/global.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  fonts: {
+    montSerrat: {
+      bold: 'Montserrat-Bold',
+      regular: 'Montserrat-Regular',
+      medium: 'Montserrat-Medium',
+    },
+    roboto: {
+      bold: 'Roboto-Bold',
+      regular: 'Roboto-Regular',
+      medium: 'Roboto-Medium',
+    },
+  },
+  fontSize: {
+    h1: 28,
+    p: 16,
+    span: 14,
+  },
+  colors: {
+    white: '#fff',
+    black: '#1e1e1e',
+  },
+  spacing: {
+    big: 16,
+    regular: 8,
+    small: 6,
+  },
+};

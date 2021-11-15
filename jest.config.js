f543fa4eab05d2efbaa4019e6457136ef6d46d40
Igor Moraes <igormoraesdev@gmail.com>
2021-11-15 18:20:25 +0000

    chore: change jest folder

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   preset: 'react-native',
   moduleDirectories: ['node_modules', 'src'],
-  setupFiles: ['<rootDir>/jest/setup.ts'],
+  setupFiles: ['<rootDir>/src/main/config/jest/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/jest/assetsTransformer.ts',
+      '<rootDir>/src/main/config/jest/assetsTransformer.ts',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|@react-native-vector-icons)',

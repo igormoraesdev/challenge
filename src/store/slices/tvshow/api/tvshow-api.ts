88cbd8000aabe2ecf1586485c46e2521c83798f8
Igor Moraes <igormoraesdev@gmail.com>
2021-11-15 22:51:45 +0000

    chore: update thunk name

--- src/store/slices/tvshow/api/tvshow-api.ts
+++ src/store/slices/tvshow/api/tvshow-api.ts
@@ -3,7 +3,7 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {makeRemoteLoadEpisodes} from '../../../../main/factories/usecases';
 
 export const fetchEpisodes = createAsyncThunk(
-  'users/fetchByIdStatus',
+  'tvShow/fetchEpisodes',
   async () => {
     const response = await makeRemoteLoadEpisodes('6771').load();
     return response;
